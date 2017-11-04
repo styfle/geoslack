f2cb40faedd4308a5be8d5fbc44ceee063e3b1be
Steven <steven@ceriously.com>
2017-11-03 22:47:37 -0400

    Add auto update feature to fix #5

--- public/js/findme.js
+++ public/js/findme.js
@@ -1,68 +1,97 @@
-function success(position) {
-  const s = document.querySelector('#status');
+const username = document.querySelector('#username');
+const autoupdate = document.querySelector('#autoupdate');
+const status = document.querySelector('#status');
+const submit = document.querySelector('#submit');
+const finding = document.querySelector('#finding');
+const mapcanvas = document.querySelector('#mapcanvas');
 
-  if (s.className === 'success') {
+const { geolocation } = navigator;
+
+function success(position) {
+  const user = username.value;
+
+  const { maps } = google;
+  const { latitude, longitude, accuracy } = position.coords;
+
+  if (status.className === 'success') {
     return;
   }
 
-  s.innerHTML = 'found you!';
-  s.className = 'success';
+  status.textContent = 'found you!';
+  status.className = 'success';
+  mapcanvas.style.display = 'block';
 
-  const mapcanvas = document.createElement('div');
-  mapcanvas.id = 'mapcanvas';
-  mapcanvas.style.height = '400px';
-  mapcanvas.style.width = '560px';
-
-  document.querySelector('article').appendChild(mapcanvas);
-
-  const latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  const latlng = new maps.LatLng(latitude, longitude);
+  
   const mapOptions = {
     zoom: 15,
     center: latlng,
     mapTypeControl: false,
-    navigationControlOptions: { style: google.maps.NavigationControlStyle.SMALL },
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    navigationControlOptions: { style: maps.NavigationControlStyle.SMALL },
+    mapTypeId: maps.MapTypeId.ROADMAP
   };
-  const map = new google.maps.Map(mapcanvas, mapOptions);
 
-  const marker = new google.maps.Marker({
+  const map = new maps.Map(mapcanvas, mapOptions);
+
+  const marker = new maps.Marker({
       position: latlng,
-      map: map,
-      title: `You are here! (within a ${position.coords.accuracy} meter radius)`
+      title: `You are here! (within a ${accuracy} meter radius)`,
+      label: user
   });
 
-  const user = document.querySelector('#username').value;
-  const data = { lat: position.coords.latitude, lng: position.coords.longitude, user: user };
+  marker.setMap(map);
+
   fetch('/coords', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data)
-  }).then(console.log).catch(console.error);
+    body: JSON.stringify({
+      lat: latitude,
+      lng: longitude,
+      user: user
+    })
+  })
+  .then(o => o.text())
+  .then(o => console.log('Server says ', o))
+  .catch(console.error);
 }
 
 function error(msg) {
-  const s = document.querySelector('#status');
-  s.innerHTML = typeof msg === 'string' ? msg : 'failed';
-  s.className = 'fail';
+  status.innerHTML = typeof msg === 'string' ? msg : 'failed';
+  status.className = 'fail';
+}
+
+function getLocation() {
+  finding.style.display = 'block';
+  submit.style.display = 'none';
+  status.textContent = 'checking...';
+  status.className = '';
+  if (geolocation) {
+    geolocation.getCurrentPosition(success, error);
+  } else {
+    error('not supported');
+  }
 }
 
 function init() {
-  const submit = document.querySelector('#submit');
-  submit.addEventListener('click', () => {
-    const finding = document.querySelector('#finding');
-    finding.style.display = 'block';
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(success, error);
-    } else {
-      error('not supported');
-    }
-  });
-
-  const username = document.querySelector('#username');
   username.value = localStorage.getItem('geoslack-username');
+  autoupdate.checked = localStorage.getItem('geoslack-autoupdate');
 
   username.addEventListener('change', () => {
     localStorage.setItem('geoslack-username', username.value);
+  });
+
+  autoupdate.addEventListener('change', () => {
+    localStorage.setItem('geoslack-autoupdate', autoupdate.checked);
+  });
+  
+  submit.addEventListener('click', () => {
+    getLocation();
+
+    setInterval(() => {
+      if (autoupdate.checked) {
+        getLocation();
+      }
+    }, 1000 * 60 * 10); // 10 minutes
   });
 }
 
